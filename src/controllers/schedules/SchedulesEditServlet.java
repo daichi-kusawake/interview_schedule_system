5b94a63d580cafd45936806f7c28d95adf72fc61
Daichi Kusawake <itemsretail@gmail.com>
2021-06-24 09:38:55 +0900

    Add Schedules show

--- src/controllers/schedules/SchedulesEditServlet.java
+++ src/controllers/schedules/SchedulesEditServlet.java
@@ -1,19 +1,27 @@
 package controllers.schedules;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import models.Schedule;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class SchedulesEditServlet
  */
 @WebServlet("/schedules/edit")
 public class SchedulesEditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +30,31 @@ public class SchedulesEditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Schedule s = em.find(Schedule.class, Integer.parseInt(request.getParameter("id")));
+
+        // 社員名一覧表示のため部署情報テーブルのデータを取得する
+        List<Employee> employees = em.createNamedQuery("getAllEmployeesNotDeleted", Employee.class)
+                .getResultList();
+
+        em.close();
+
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(s != null && login_employee.getId() == s.getEmployee().getId()) {
+            request.setAttribute("employees", employees);
+
+            request.setAttribute("schedule", s);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("schedule_id", s.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/schedules/edit.jsp");
+        rd.forward(request, response);
+    }
 
 }
